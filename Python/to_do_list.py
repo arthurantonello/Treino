927dd4c704f4ef07898904168a310266f86ea135
Arthur Antonello <arthur.antonello@hotmail.com>
2025-04-07 13:18:43 -0300

    Update to_do_list.py

--- Python/to_do_list.py
+++ Python/to_do_list.py
@@ -1,7 +1,10 @@
 import os
+import datetime
 os.system('cls')
 
-# Lista de usuários vazia para programa do zero
+hoje = datetime.datetime.today()
+data_padrao = hoje.strftime("%d-%m-%Y") #Inserido uma data padrão caso usuário não informe
+# Lista de usuários vazia para programa zerado
 # usuarios = {}
 
 # Lista de usuários pré existentes para teste
@@ -9,22 +12,38 @@ usuarios = {
     "email@mail": {
         "nome": "Arthur",
         "idade": 25,
-        "tarefas": ["Revisar código", "Responder e-mails", "Participar da daily"]
+        "tarefas": [
+            {"descricao": "Revisar código", "data": data_padrao},
+            {"descricao": "Responder e-mails", "data": data_padrao},
+            {"descricao": "Participar da daily", "data": data_padrao}
+        ]
     },
     "user1@email.com": {
         "nome": "Alice",
         "idade": 30,
-        "tarefas": ["Estudar Python", "Praticar exercícios", "Assistir aula gravada"]
+        "tarefas": [
+            {"descricao": "Estudar Python", "data": data_padrao},
+            {"descricao": "Praticar exercícios", "data": data_padrao},
+            {"descricao": "Assistir aula gravada", "data": data_padrao}
+        ]
     },
     "user2@email.com": {
         "nome": "Bruno",
         "idade": 22,
-        "tarefas": ["Refatorar função", "Atualizar documentação", "Testar novas features"]
+        "tarefas": [
+            {"descricao": "Refatorar função", "data": data_padrao},
+            {"descricao": "Atualizar documentação", "data": data_padrao},
+            {"descricao": "Testar novas features", "data": data_padrao}
+        ]
     },
     "user3@email.com": {
         "nome": "Carla",
         "idade": 27,
-        "tarefas": ["Comer", "Beber água", "Jogar"]
+        "tarefas": [
+            {"descricao": "Comer", "data": data_padrao},
+            {"descricao": "Beber água", "data": data_padrao},
+            {"descricao": "Jogar", "data": data_padrao}
+        ]
     }
 }
 
@@ -35,18 +54,37 @@ def usuario_existe(chave):
     else:
         return True
 
-
 def cadastrar_usuario():
     nome = input('Insira o nome do novo usuário: ')
     idade = int(input('Insira a idade: '))
     email = input('Insira o e-mail: ')
     usuarios[email] = {"nome": nome, "idade": idade}
 
-def adicionar_tarefa(chave,tarefa):
+def adicionar_tarefa(chave):
+    tarefa = input('Insira a tarefa para adicionar: ')
+    print('Digite a data da tarefa no formato DD-MM-YYYY,')
+    data_str = input('Digite a data da tarefa no formato DD-MM-YYYY,\nou aperte enter para a data ser hoje: ')
+    # Se não foi informada data, a padrão é instaurada e logo é formatado em ambas situações
+    if not data_str:
+        data_str = data_padrao
+    data = datetime.datetime.strptime(data_str, '%d-%m-%Y') 
+
+    # Para não haver usuário sem tarefas, uma em branco é inicializada caso já não tenha
     if "tarefas" not in usuarios[chave]:
             usuarios[chave]["tarefas"] = []
-    usuarios[chave]["tarefas"].append(tarefa)
+    nova_tarefa = {
+        "descricao" : tarefa,
+        "data" : data
+    }
+    usuarios[chave]["tarefas"].append(nova_tarefa)
     print("Tarefa adicionada com sucesso")
+
+def listar_tarefas(chave):
+    if usuario_existe(chave):
+        print(f"Tarefas do usuário {usuarios[chave]['nome']}\n")
+        for tarefas in usuarios[chave]['tarefas']: 
+            print(f"- {tarefas['descricao']} - {tarefas['data']}")
+        print("\n -------------\n")
 
 while True:
     print("\n=== Sistema de Cadastro ===")
@@ -64,42 +102,28 @@ while True:
     if opcao == 2:
         chave = input('Insira o e-mail do usuário para adicionar a tarefa: ')
         if usuario_existe(chave):
-            tarefa = input('Insira a tarefa para adicionar: ')
-            adicionar_tarefa(chave, tarefa)
+            adicionar_tarefa(chave)
             
-
     if opcao == 3:
         chave = input('Insira o e-mail do usuário para listar as tarefa: ')
-        if usuario_existe(chave):
-            print("\n -------------\n")
-            print(f"Tarefas do usuário {usuarios[chave]['nome']}\n")
-            for tarefas in usuarios[chave]["tarefas"]: 
-                print(f"- {tarefas}")
-            print("\n -------------\n")
+        listar_tarefas(chave)
 
     if opcao == 4:
         chave = input('Insira o e-mail usuário para remover a tarefa: ')
-        if usuario_existe(chave):
-            print(f"Tarefas do usuário {usuarios[chave]['nome']}")
-            for tarefas in usuarios[chave]["tarefas"]: 
-                print(f"- {tarefas}")
-            tarefa = input('Qual tarefa gostaria de remover? ')
-            if tarefa in usuarios[chave]["tarefas"]:
-                usuarios[chave]["tarefas"].remove(tarefa)
-                print(f"Tarefa {tarefa} removida com sucesso")
-            else:
-                print(f"Tarefa {tarefa} não encontrada")
+        listar_tarefas(chave)
+        tarefa = input('Qual tarefa gostaria de remover? ')
+        if tarefa in usuarios[chave]["tarefas"]:
+            usuarios[chave]["tarefas"].remove(tarefa)
+            print(f"Tarefa {tarefa} removida com sucesso")
+        else:
+            print(f"Tarefa {tarefa} não encontrada")
 
     if opcao == 5:
         for email, usuario in usuarios.items():
             print(f"Usuário: {usuario['nome']} - Idade: {usuario['idade']} - E-mail: {email}")
-            print("Tarefas:")
             if "tarefas" not in usuario:
                 print("-- Sem tarefas --")
             else: 
-                for tarefa in usuario["tarefas"]: 
-                    print(f"- {tarefa}")
-            print("---------------")
+                listar_tarefas(email)
     if opcao == 6:
         break
-
